e45f60b7d2f2463bf3e2d623b4c3e48bf3ddb40a
JagadeeshSevn3Ai <111954833+JagadeeshSevn3Ai@users.noreply.github.com>
2023-05-08 22:19:13 +0530

    Update util.js

--- src/component/UploadDocument/util.js
+++ src/component/UploadDocument/util.js
@@ -1,6 +1,6 @@
 export const containerName = `pdffileuploadv1`;
-export const sasToken = process.env.REACT_APP_STORAGESASTOKEN;
-export const storageAccountName = process.env.REACT_APP_STORAGERESOURCENAME; 
+export const sasToken = '?sv=2021-12-02&ss=bf&srt=sco&sp=rwdlaciytfx&se=2023-12-10T23:55:11Z&st=2023-04-11T15:55:11Z&spr=https&sig=7fEw2VoDxfm97inyQosDMyakOb9QIhNS2Naw%2BEUuAIo%3D';
+export const storageAccountName = 'https://documentuploadstrg.blob.core.windows.net/?sv=2021-12-02&ss=bf&srt=sco&sp=rwdlaciytfx&se=2023-12-10T23:55:11Z&st=2023-04-11T15:55:11Z&spr=https&sig=7fEw2VoDxfm97inyQosDMyakOb9QIhNS2Naw%2BEUuAIo%3D'; 
 
 export const isStorageConfigured = () => {
   return (!storageAccountName || !sasToken) ? false : true;
